2f6e2e88979fd0521145aa3c4f30a58a41b23d21
Giuseppe Capizzi <g.capizzi@gmail.com>
2011-05-07 09:45:18 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marley::Application.config.secret_token = 'e55d74d829ed7ff47bbdfbca7871c3cd15b0c4e476b4190c7442e37a79a0ddb20f90990689b6d76b86f72672fa80e6a0242d9407b436ff2b102c2d84c9a2ea10'
