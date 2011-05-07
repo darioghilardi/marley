44f4caeb9b4ce1bd1e4370a7dc95a1ad3df3144e
Giuseppe Capizzi <g.capizzi@gmail.com>
2011-05-07 12:55:56 +0200

    Added team memberships

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,6 @@
 class Team < ActiveRecord::Base
   has_many :projects
+
+  has_many :memberships
+  has_many :users, :through => :memberships
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110507083435) do
+ActiveRecord::Schema.define(:version => 20110507104248) do
+
+  create_table "memberships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.boolean  "admin"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "name"
