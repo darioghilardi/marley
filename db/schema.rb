c62ad96e4167e1e914c6f96c86b6acf502b4b763
Giuseppe Capizzi <g.capizzi@gmail.com>
2011-05-08 17:16:52 +0200

    Assigments schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110507104248) do
+ActiveRecord::Schema.define(:version => 20110508141716) do
+
+  create_table "assignments", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "task_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "memberships", :force => true do |t|
     t.integer  "user_id"
