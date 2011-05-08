aa87878c6e131f3f5b4e28759de4a77d0d3fe350
Giuseppe Capizzi <g.capizzi@gmail.com>
2011-05-08 18:18:43 +0200

    Moved menu to partial. Added menu_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def menu_link(title, controller)
+    link_to title, { :controller => controller }, current_page?(:controller => controller) ? { :class => "selected" } : {}
+  end
 end
