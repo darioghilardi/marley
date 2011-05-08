95171e4081e965996ee7dbacab827f8b78e18921
Giuseppe Capizzi <g.capizzi@gmail.com>
2011-05-08 19:11:27 +0200

    Added name field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110508141716) do
+ActiveRecord::Schema.define(:version => 20110508163034) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "user_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20110508141716) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "superadmin"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
